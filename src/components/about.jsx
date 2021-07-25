895db9c455466ce71d454899cd09883a47697efd
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-07-25 12:34:26 -0300

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,9 +2,6 @@ export const About = (props) => {
   return (
 
     <div id="about">
-      <>
-      testando about
-      </>
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-6">
