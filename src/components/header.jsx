4676fb46bfc56be84be39ac88f1f82648fb71f6f
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-07-26 00:16:07 -0300

    added youtube iframe

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,18 @@
 export const Header = (props) => {
   return (
     <header id='header'>
-      <div className='intro'>
-        <div className='overlay'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-8 col-md-offset-2 intro-text'>
-              <iframe width="560" height="400" src="https://www.youtube.com/embed/joG5hrkQsZw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <a
-                  href='#features'
-                  className='btn btn-custom btn-lg page-scroll'
-                >
-                  Learn More
-                </a>{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className='intro'>
+    <div className='overlay'>
+    <div className='container'>
+    <div className='row'>
+    <div class="youtube-video-container">
+    <iframe src="https://www.youtube.com/embed/joG5hrkQsZw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    </div>
+    </div>
+    </div>
+    </div>
     </header>
-  )
-}
+    )
+  }
