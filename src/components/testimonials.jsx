7e40303a9f9f73975191c76743d8ecbdaa496bb7
Leticia Felix <leticiafs@hotmail.com.br>
2021-07-25 23:04:32 -0300

    implementei mapa no componente testimonials

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -1,27 +1,78 @@
-export const Testimonials = (props) => {
+import React from 'react';
+import { GoogleMap, useJsApiLoader} from "@react-google-maps/api"
+
+// const libraries = ["places"];
+
+const containerStyle = {
+  width: '40vw',
+  height: '50vh'
+};
+// const center = {
+//   lat: 41.40338,
+//   lng: 2.17403,
+// };
+
+// const options = {
+//   styles: mapStyles,
+//   disableDefaultUI: true,
+//   zoomControl: true,
+// }
+
+export const Testimonials = () => {
+ 
+ const {isLoaded} = useJsApiLoader({
+      id: 'google-map-script',
+      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+      // libraries,
+    });
+    
+  
+
+  const GoogleMapsComponent = () => {
+    const [map, setMap] = React.useState(null)
+
+  
+    // if (loadError) return "Error loading maps";
+    // if (!isLoaded) return "Loading Maps";
+        const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds();
+        map.fitBounds(bounds);
+        setMap(map)
+      }, [])
+    
+      const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+      }, [])
+  
+
+    return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        // center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+      ) : <></>
+
+  }
+  
+
   return (
     <div id='testimonials'>
-      <div className='container'>
-        <div className='section-title text-center'>
-          <h2>What our clients say</h2>
-        </div>
+      <div className="container">
         <div className='row'>
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-4'>
-                  <div className='testimonial'>
-                    <div className='testimonial-image'>
-                      {' '}
-                      <img src={d.img} alt='' />{' '}
-                    </div>
-                    <div className='testimonial-content'>
-                      <p>"{d.text}"</p>
-                      <div className='testimonial-meta'> - {d.name} </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : 'loading'}
+          <div className='"col-xs-12 col-sm-4 col-md-6"'>
+            <h2>Plantei uma muda para o desafio Terra Doce Lar</h2>
+            <p>Queremos lembrar do poder individual de transformação exponencial que temos quando nos conectamos ao coletivo.</p>
+            <p>Acreditamos em um mundo em que os alimentos são gratuitos, abundantes e disponíveis para todos os seres do planeta.</p>
+          </div>
+          <div className='"col-xs-12 col-sm-6 col-md-6"'>
+          <GoogleMapsComponent/>
+          </div>
         </div>
       </div>
     </div>
