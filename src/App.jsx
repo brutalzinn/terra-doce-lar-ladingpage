9449779059f315012af8e5586483905f0dec884a
Suelen dos Santos Batista <suelen.batista@al.infnet.edu.br>
2021-07-26 01:29:25 -0300

    ultimo do dia funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,15 @@ import { Features } from "./components/features";
 import { About } from "./components/about";
 import { Ourstore } from "./components/ourstore";
 import { Gallery } from "./components/gallery";
-<<<<<<< HEAD
 import { Testimonials } from "./components/testimonials";
-// import { Team } from "./components/team";
-=======
 import { Overallnumber } from "./components/overallnumber";
 import { Team } from "./components/team";
->>>>>>> suelen
 import { Contact } from "./components/contact";
 import { Footer } from "./components/footer";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -38,12 +35,8 @@ const App = () => {
       <About data={landingPageData.About} />
       <Ourstore data={landingPageData.Ourstore} />
       <Gallery />
-<<<<<<< HEAD
       <Testimonials data={landingPageData.Testimonials} />
-      {/* <Team data={landingPageData.Team} /> */}
-=======
       <Team data={landingPageData.Team} />
->>>>>>> suelen
       <Contact data={landingPageData.Contact} />
       <Footer data={landingPageData.Footer} />
     </div>
--- src/components/overallnumber.jsx
+++ src/components/overallnumber.jsx
@@ -1,80 +1,4 @@
-<<<<<<< HEAD:src/components/testimonials.jsx
-import React from 'react';
-import { GoogleMap, useJsApiLoader} from "@react-google-maps/api"
 
-// const libraries = ["places"];
-
-const containerStyle = {
-  width: '40vw',
-  height: '50vh'
-};
-// const center = {
-//   lat: 41.40338,
-//   lng: 2.17403,
-// };
-
-// const options = {
-//   styles: mapStyles,
-//   disableDefaultUI: true,
-//   zoomControl: true,
-// }
-
-export const Testimonials = () => {
- 
- const {isLoaded} = useJsApiLoader({
-      id: 'google-map-script',
-      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-      // libraries,
-    });
-    
-  
-
-  const GoogleMapsComponent = () => {
-    const [map, setMap] = React.useState(null)
-
-  
-    // if (loadError) return "Error loading maps";
-    // if (!isLoaded) return "Loading Maps";
-        const onLoad = React.useCallback(function callback(map) {
-        const bounds = new window.google.maps.LatLngBounds();
-        map.fitBounds(bounds);
-        setMap(map)
-      }, [])
-    
-      const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-      }, [])
-  
-
-    return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        // center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-      ) : <></>
-
-  }
-  
-
-  return (
-    <div id='testimonials'>
-      <div className="container">
-        <div className='row'>
-          <div className='"col-xs-12 col-sm-4 col-md-6"'>
-            <h2>Plantei uma muda para o desafio Terra Doce Lar</h2>
-            <p>Queremos lembrar do poder individual de transformação exponencial que temos quando nos conectamos ao coletivo.</p>
-            <p>Acreditamos em um mundo em que os alimentos são gratuitos, abundantes e disponíveis para todos os seres do planeta.</p>
-          </div>
-          <div className='"col-xs-12 col-sm-6 col-md-6"'>
-          <GoogleMapsComponent/>
-          </div>
-=======
 export const Overallnumber = (props) => {
   return (
     <div id='overallnumber'>
@@ -98,7 +22,6 @@ export const Overallnumber = (props) => {
                 </div>
               ))
             : 'loading'}
->>>>>>> suelen:src/components/overallnumber.jsx
         </div>
       </div>
     </div>
